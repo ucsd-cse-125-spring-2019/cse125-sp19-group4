37faa54d5007bfd9e8b9293707578e74d0453905
Shuyuan Ma <shm025@ucsd.edu>
2019-04-16 16:05:49 -0700

    free camera with multiple objects demo

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,8 @@
+// ============================ Network IO ================================
+
 const socket = io();
 
 let command = '';
-
-socket.on('broadcast', function(obj) {
-    console.log('msg from server: ' + obj.number);
-    console.log('msg from server: ' + obj.string);
-    console.log('msg from server: ' + obj.array);
-    console.log('msg from server: ' + obj.myObj.name);
-});
-
 
 socket.on('chat message', function(msg){
     $('#messages').append($('<li>').text(msg));
@@ -20,13 +14,45 @@ socket.on('chat message', function(msg){
 });
 
 $('.game-area').html($('#intro-screen-template').html());
-/*
-====================================Canvas===================================
-*/
+
+
+socket.on('role already taken', function(msg){
+    alert(msg);
+});
+
+socket.on('enter game', function(){
+    console.log('enter game');
+    $('.game-area').html($('#ingame-template').html());
+    $('form').submit(function(e){
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    main();
+});
+
+socket.on('wait for game begin', function(msg){
+    $('.game-area').html($('#loading-screen-template').html());
+    $('#queue').html(msg);
+});
+
+$('#GodButton').click(function(){
+    socket.emit("play as god");
+});
+
+$('#SurvivorButton').click(function(){
+    socket.emit("play as survivor");
+});
+
+
+// ====================================Canvas===================================
+
 import getWrappedGL from '/public/util/debug.js';
 import readStringFrom from '/public/util/io.js';
+import Camera from '/public/js/camera.js'
 
-let cubeRotation = 0.0;
+const camera = new Camera();
 
 /**
  * Start here
@@ -40,6 +66,13 @@ function main() {
     canvas.height = 0.8 * window.innerHeight;
     console.log('width', canvas.width, 'height', canvas.height);
 
+    canvas.addEventListener("mousedown", mouseDown, false);
+    canvas.addEventListener("mouseup", mouseUp, false);
+    canvas.addEventListener("mouseout", mouseUp, false);
+    canvas.addEventListener("mousemove", mouseMove, false);
+    //window.addEventListener("keydown", keyDown, false);
+    window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+    window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
 
     /** @type {WebGLRenderingContext} */
     const gl = getWrappedGL(canvas);
@@ -72,26 +105,38 @@ function main() {
         },
     };
 
-    // Here's where we call the routine that builds all the
-    // objects we'll be drawing.
-    const buffers = initBuffers(gl);
+    // Here's where we call the routine that builds all the objects we'll be drawing.
+    // const buffers = initCubeBuffers(gl);
+    const castle_mesh = initMesh(gl, "/public/model/castle.obj");
+    const male_mesh = initMesh(gl, "/public/model/male.obj");
+    let trans_left = glMatrix.mat4.create();
+    glMatrix.mat4.fromTranslation(trans_left, [5, 0, 0]);
+    let trans_right = glMatrix.mat4.create();
+    glMatrix.mat4.fromTranslation(trans_right, [0, 0, 0]);
+    const meshes = [{'m': male_mesh, 't':trans_left},
+                    {'m': male_mesh, 't':trans_right},
+                    {'m': castle_mesh, 't':glMatrix.mat4.create()}];
 
     let then = 0;
-
     // Draw the scene repeatedly
     function render(now) {
-        now *= 0.001;  // convert to seconds
+        now *= 0.001;
         const deltaTime = now - then;
         then = now;
+            
+        if (Key.isDown('UP')) camera.moveUp(deltaTime);
+        if (Key.isDown('DOWN')) camera.moveDown(deltaTime);
+        if (Key.isDown('LEFT')) camera.moveLeft(deltaTime);
+        if (Key.isDown('RIGHT')) camera.moveRight(deltaTime);
+        camera.updateCameraVectors();
 
         if (command == 'cube') {
-            drawScene(gl, programInfo, buffers, deltaTime);
+            drawScene(gl, programInfo, meshes, camera);
         } else {
             gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
             gl.clearDepth(1.0);                 // Clear everything
 
             // Clear the canvas before we start drawing on it.
-
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         }
 
@@ -100,126 +145,34 @@ function main() {
     requestAnimationFrame(render);
 }
 
-
 /**
- * Initialize the buffers we'll need. For this demo, we just have one object -- 
- * a simple three-dimensional cube.
+ * Initialize the buffers of the mesh
  * @param  {WebGLRenderingContext} gl
+ * @param  {String} filepath
  */
-function initBuffers(gl) {
-    // Create a buffer for the cube's vertex positions.
-    /** @type {WebGLBuffer} */
-    const positionBuffer = gl.createBuffer();
-
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Now create an array of positions for the cube.
-
-    const positions = [
-        // Front face
-        -1.0, -1.0, 1.0,
-        1.0, -1.0, 1.0,
-        1.0, 1.0, 1.0,
-        -1.0, 1.0, 1.0,
-
-        // Back face
-        -1.0, -1.0, -1.0,
-        -1.0, 1.0, -1.0,
-        1.0, 1.0, -1.0,
-        1.0, -1.0, -1.0,
-
-        // Top face
-        -1.0, 1.0, -1.0,
-        -1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0,
-        1.0, 1.0, -1.0,
-
-        // Bottom face
-        -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-        1.0, -1.0, 1.0,
-        -1.0, -1.0, 1.0,
-
-        // Right face
-        1.0, -1.0, -1.0,
-        1.0, 1.0, -1.0,
-        1.0, 1.0, 1.0,
-        1.0, -1.0, 1.0,
-
-        // Left face
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0, 1.0,
-        -1.0, 1.0, 1.0,
-        -1.0, 1.0, -1.0,
-    ];
-
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    // Now set up the colors for the faces. We'll use solid colors
-    // for each face.
-
-    const faceColors = [
-        [255, 255, 255, 255],    // Front face: white
-        [255, 0, 0, 255],    // Back face: red
-        [0, 255, 0, 255],    // Top face: green
-        [0, 0, 255, 255],    // Bottom face: blue
-        [255, 255, 0, 255],    // Right face: yellow
-        [255, 0, 255, 255],    // Left face: purple
-    ];
-
-    // Convert the array of colors into a table for all the vertices.
-
-    let colors = [];
-
-    for (let j = 0; j < faceColors.length; ++j) {
-        const c = faceColors[j];
-
-        // Repeat each color four times for the four vertices of the face
-        colors = colors.concat(c, c, c, c);
+function initMesh(gl, filepath) {
+    //load model
+    function readTextFile(file) {
+        let text;
+        const rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status == 0) {
+                    text = rawFile.responseText;
+                }
+            }
+        }
+        rawFile.send(null);
+        return text;
     }
 
-    /** @type {WebGLBuffer} */
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colors), gl.STATIC_DRAW);
+    const male_text = readTextFile(filepath);
 
-    // Build the element array buffer; this specifies the indices
-    // into the vertex arrays for each face's vertices.
+    const mesh = new OBJ.Mesh(male_text);
+    OBJ.initMeshBuffers(gl, mesh);
 
-    /** @type {WebGLBuffer} */
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-
-    const indices = [
-        0, 1, 2, 0, 2, 3,    // front
-        4, 5, 6, 4, 6, 7,    // back
-        8, 9, 10, 8, 10, 11,   // top
-        12, 13, 14, 12, 14, 15,   // bottom
-        16, 17, 18, 16, 18, 19,   // right
-        20, 21, 22, 20, 22, 23,   // left
-    ];
-
-    // Now send the element array to GL
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices), gl.STATIC_DRAW);
-
-    return {
-        position: positionBuffer,
-        color: colorBuffer,
-        indices: indexBuffer,
-    };
+    return mesh;
 }
 
 
@@ -227,10 +180,11 @@ function initBuffers(gl) {
  * Draw the scene.
  * @param  {WebGLRenderingContext} gl
  * @param  {Object} programInfo
- * @param  {WebGLBuffer} buffers
- * @param  {Number} deltaTime
+ * @param  {Array} meshes mashes from initMesh(gl, filename) with 
+ * transformation
+ * @param  {Camera} camera
  */
-function drawScene(gl, programInfo, buffers, deltaTime) {
+function drawScene(gl, programInfo, meshes, camera) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -263,97 +217,77 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 
     // Set the drawing position to the "identity" point, which is
     // the center of the scene.
-    const modelViewMatrix = glMatrix.mat4.create();
+    const modelViewMatrix = camera.getViewMatrix();
 
     // Now move the drawing position a bit to where we want to
     // start drawing the square.
+    meshes.forEach(function (mesh) {
+        let modelView = glMatrix.mat4.create();
+        glMatrix.mat4.multiply(modelView, modelViewMatrix, mesh.t);
 
-    // TODO: camera matrix
+        // Tell WebGL how to pull out the positions from the position
+        // buffer into the vertexPosition attribute
+        {
+            const numComponents = mesh.m.vertexBuffer.itemSize;
+            const type = gl.FLOAT;
+            const normalize = false;
+            const stride = 0;
+            const offset = 0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, mesh.m.vertexBuffer);
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexPosition,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset);
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexPosition);
+        }
 
-    glMatrix.mat4.translate(modelViewMatrix,     // destination matrix
-        modelViewMatrix,     // matrix to translate
-        [-0.0, 0.0, -6.0]);  // amount to translate
-    glMatrix.mat4.rotate(modelViewMatrix,  // destination matrix
-        modelViewMatrix,  // matrix to rotate
-        cubeRotation,     // amount to rotate in radians
-        [0, 0, 1]);       // axis to rotate around (Z)
-    glMatrix.mat4.rotate(modelViewMatrix,  // destination matrix
-        modelViewMatrix,  // matrix to rotate
-        cubeRotation,// amount to rotate in radians
-        [0, 1, 0]);       // axis to rotate around (X)
-    glMatrix.mat4.rotate(modelViewMatrix,  // destination matrix
-        modelViewMatrix,  // matrix to rotate
-        cubeRotation,// amount to rotate in radians
-        [1, 0, 0]);       // axis to rotate around (Y)
+        // Tell WebGL how to pull out the colors from the color buffer
+        // into the vertexColor attribute.
+        {
+            const numComponents = mesh.m.normalBuffer.itemSize;
+            const type = gl.FLOAT;
+            const normalize = false;
+            const stride = 0;
+            const offset = 0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, mesh.m.normalBuffer);
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexColor,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset);
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexColor);
+        }
 
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute
-    {
-        const numComponents = 3;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexPosition);
-    }
+        // Tell WebGL which indices to use to index the vertices
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.m.indexBuffer);
 
-    // Tell WebGL how to pull out the colors from the color buffer
-    // into the vertexColor attribute.
-    {
-        const numComponents = 4;
-        const type = gl.UNSIGNED_BYTE;
-        const normalize = true;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexColor,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexColor);
-    }
+        // Tell WebGL to use our program when drawing
+        gl.useProgram(programInfo.program);
 
-    // Tell WebGL which indices to use to index the vertices
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+        // Set the shader uniforms
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.projectionMatrix,
+            false,
+            projectionMatrix);
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.modelViewMatrix,
+            false,
+            modelView);
 
-    // Tell WebGL to use our program when drawing
-
-    gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.projectionMatrix,
-        false,
-        projectionMatrix);
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix);
-
-    {
-        const vertexCount = 36;
-        const type = gl.UNSIGNED_SHORT;
-        const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
-
-    // Update the rotation for the next draw
-
-    cubeRotation += deltaTime;
+        {
+            const vertexCount = mesh.m.indexBuffer.numItems;
+            const type = gl.UNSIGNED_SHORT;
+            const offset = 0;
+            gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        }
+    })
 }
 
 
@@ -370,14 +304,12 @@ function initShaderProgram(gl, vsFilename, fsFilename) {
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
     // Create the shader program
-
     const shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
 
     // If creating the shader program failed, alert
-
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
         alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
         return null;
@@ -398,15 +330,12 @@ function loadShader(gl, type, source) {
     const shader = gl.createShader(type);
 
     // Send the source to the shader object
-
     gl.shaderSource(shader, source);
 
     // Compile the shader program
-
     gl.compileShader(shader);
 
     // See if it compiled successfully
-
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
@@ -416,32 +345,60 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
-socket.on('role already taken', function(msg){
-    alert(msg);
-});
+/*================= Mouse events ======================*/
 
-socket.on('enter game', function(){
-    console.log('enter game');
-    $('.game-area').html($('#ingame-template').html());
-    $('form').submit(function(e){
-        e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
-    });    
+let drag = false;
+let old_x, old_y;
 
-    main();
-});
 
-socket.on('wait for game begin', function(msg){
-    $('.game-area').html($('#loading-screen-template').html());
-    $('#queue').html(msg);
-});
+const mouseDown = function (e) {
+    drag = true;
+    old_x = e.pageX, old_y = e.pageY;
+    e.preventDefault();
+    return false;
+};
 
-$('#GodButton').click(function(){
-    socket.emit("play as god");
-});
+const mouseUp = function (e) {
+    drag = false;
+};
 
-$('#SurvivorButton').click(function(){
-    socket.emit("play as survivor");
-});
+const mouseMove = function (e) {
+    if (!drag) return false;
+    camera.ProcessMouseMovement(e.pageX - old_x, e.pageY - old_y);
+    camera.updateCameraVectors();
+    old_x = e.pageX, old_y = e.pageY;
+    e.preventDefault();
+};
+
+
+const Key = {
+    _pressed: {},
+
+    cmd: {
+        37: 'LEFT',     // left arrow
+        38: 'UP',       // up arrow
+        39: 'RIGHT',    // right arrow
+        40: 'DOWN',     // down arrow
+        65: 'LEFT',     // A
+        68: 'RIGHT',    // D
+        83: 'DOWN',     // S
+        87: 'UP',       // W
+    },
+    
+    isDown: function(command) {
+        return this._pressed[command];
+    },
+    
+    onKeydown: function(event) {
+        if (event.keyCode in this.cmd) {
+            this._pressed[this.cmd[event.keyCode]] = true;
+        }
+    },
+    
+    onKeyup: function(event) {
+        if (event.keyCode in this.cmd) {
+            delete this._pressed[this.cmd[event.keyCode]];
+        }
+    }
+};
+
