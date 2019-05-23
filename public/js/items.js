d0d0c460679708de9724428b7c48c72690728dc9
Tianhui Cao <t9cao@ucsd.edu>
2019-05-22 21:04:09 -0700

    Add new items

--- public/js/items.js
+++ public/js/items.js
@@ -6,7 +6,28 @@ items = {
         enhance(buff) {
             buff.STATUS_speed += 1;
         },
-    }
+    },
+    'swords': {
+        count: 0,
+        description: "each sword you have will increase your attack by 1",
+        enhance(buff) {
+            buff.STATUS_damage += 1;
+        },
+    },
+    'shields': {
+        count: 0,
+        description: "each shield you have will increase your defence by 1",
+        enhance(buff) {
+            buff.STATUS_defense += 1;
+        },
+    },
+    'hearts': {
+        count: 0,
+        description: "each heart you have will increase your health by 10",
+        enhance(buff) {
+            buff.STATUS_maxHealth += 10;
+        },
+    },
 }
 
 module.exports = items;
