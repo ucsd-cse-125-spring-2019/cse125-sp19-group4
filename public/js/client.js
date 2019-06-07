b88d28863e0147272b0467f7fc494341ccb908e0
Shuyuan Ma <shm025@ucsd.edu>
2019-06-07 15:34:23 -0700

    spectator show progress

--- public/js/client.js
+++ public/js/client.js
@@ -485,13 +485,14 @@ socket.on('game_status', function (msg) {
         if (typeof player.skills !== 'undefined') {
             UI.coolDownUpdate(player.skills);
         }
-        if (typeof status.progess !== 'undefine') {
-            UI.updateProgressBar(status.progress)
-        }
 
         if ('items' in player) {
             UI.updateItems(player.items)
         }
+    }
+
+    if (typeof status.progess !== 'undefine') {
+        UI.updateProgressBar(status.progress)
     }
     UI.teammatesUpdate(data);
 
